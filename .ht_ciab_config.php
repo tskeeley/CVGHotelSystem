139aa4a11be85ef11872117e8c2a417ec549d2b1
Thomas Keeley <tskeeley@gmail.com>
2015-12-14 21:21:21 -0500

    Clone of CIAB V6.0 - Starting place

---
+++ .ht_ciab_config.php
@@ -0,0 +1,23 @@
+<?php
+
+// CON-In-A-Box Site Config
+
+// Site Specific Variables
+$CONSITENAME="CONvergence Operations";
+
+// Admin User - Must have until users are set up.
+// After that, you can add an astrix to the password which will make it unmatchable (IE: Locked)
+$ADMINUSER="Admin"; // Do not leave this blank
+$ADMINPASS="sWGjBbyIZsL7Q"; // Crypt hashed password (eg: obtained via htpasswd)
+
+// DBConfig, passwords, etc
+// Do not share this info
+$DBHOST="localhost";
+$DBUSER="coninabo_dev";
+$DBNAME="coninabo_dev";
+$DBPASS="test1"; // Plain text database user's password
+
+// Timezone to use
+date_default_timezone_set("America/Chicago");
+
+?>
