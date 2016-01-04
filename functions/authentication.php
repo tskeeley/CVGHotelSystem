6538a5c3c2a49abbab298ec1d9d772a9a1355f9e
Thomas Keeley <tskeeley@gmail.com>
2016-01-04 11:41:08 -0500

    Non-functional - Authentication routines in place

--- functions/authentication.php
+++ functions/authentication.php
@@ -7,74 +7,41 @@ function LogoutUser() {
     setcookie(session_name(), '', time()-42000, '/');
   }
   session_destroy();
-}
-
-function UserAuthorized($user, $position) {
-  // Check if user is allowed to use this position
-  $PosTrim=explode("-", $position);
-  $PosCheck=$PosTrim[0];
-
-  $query="SELECT ATTENDEE_ID FROM " . $GLOBALS['AUTHENTICATIONTABLE'] . " WHERE LoginName='" . $user . "'";
-  $result=run_query($query);
-  $rows=mysql_fetch_assoc($result);
-
-  $query="SELECT Basic," . $PosCheck . " FROM " . $GLOBALS['USERSKILLSTABLE'] . " WHERE ATTENDEE_ID='" . $rows['ATTENDEE_ID'] . "'";
-  $result=run_query($query);
-
-  if (mysql_num_rows($result) == 1) {
-    $rows=mysql_fetch_assoc($result);
-    return (!empty($rows[$PosCheck]) && !empty($rows['Basic']));
-  } else {
-    return(FALSE);
-  }
-}
-
-function getCurrentUser($user=FALSE, $position) { //string(username), or False
-  if ($user) {
-    if($user == $GLOBALS['ADMINUSER']) {
-      // $ADMINUSER is allowed all postitions, Log 'em in!
-      return $user;
-    } else {
-      if (UserAuthorized($user, $position)) {
-        return $user;
-      } else {
-        return False;
-      }
-    }
-  } else {
-    return False;
-  }
+  goSite();
 }
 
 function doUserAndPasswordMatch($user, $password) {
-  if ($user == $GLOBALS['ADMINUSER']) {
-    return (crypt($password, $GLOBALS['ADMINPASS']) == $GLOBALS['ADMINPASS']);
-  }
-
-  $query="SELECT Password FROM " . $$GLOBALS['AUTHENTICATIONTABLE'] . " WHERE LoginName='" . mysql_real_escape_string($user) . "'";
-  $result=run_query($query);
-
-  $count=mysql_num_rows($result);
-  if($count > 1) {
-    die("Database error.  Got " . $count . " responses from DB when asking for user login info.  Impossible.  Check for corruption!");
-  } elseif ($count == 1) {
-    $rows=mysql_fetch_assoc($result);
-    return (crypt($password, $rows['Password']) == $rows['Password']);
-  } elseif ($count == 0) {
-    MakeLog("Failed login attempt of non-existant user");
-    return False;
+  if (!empty($GLOBALS['ADMINUSER']) && $user == $GLOBALS['ADMINUSER']) {
+    if (crypt($password, $GLOBALS['ADMINPASS']) == $GLOBALS['ADMINPASS']) {
+      $name = "Admin User";
+      return $name;
+    }
   } else {
-    die("Unexpected impossible result from database call.  Aborting!");
+
+  $request = [
+    'method' => 'common/authenticateUser', 
+    'parameters' => [
+      'username' => $user,
+      'password' => $password,
+    ],
+  ];
+  $result = $neon->go($request);
+
+  if ($response['operationResult'] == 'SUCCESS') {
+    return $response;
+  } else {
+    return FALSE;
   }
 }
 
-function CheckAdminStatus($user) {
-  // Is user allowed to access the Administration Fuctions
-  if($user == $GLOBALS['ADMINUSER']) {
+function CheckInteractiveStatus($user) {
+  // Is user allowed to access this system
+  if(!empty($GLOBALS['ADMINUSER']) && $user == $GLOBALS['ADMINUSER']) {
     // Admin user is always allowed access
     return(TRUE);
   }
 
+die("Poll Neon user to check if this user is allowed in the hotel system"); // ##==##
   $query="SELECT ATTENDEE_ID FROM " . $GLOBALS['AUTHENTICATIONTABLE'] . " WHERE LoginName='" . $user . "'";
   $result=run_query($query);
   $rows=mysql_fetch_assoc($result);
@@ -92,37 +59,23 @@ function CheckAdminStatus($user) {
 }
 
 function LoginUser($user, $password, $position) {
-  if (doUserAndPasswordMatch($user, $password)) {
-    // Log in the User
-    session_start();
-    $_SESSION['username'] = $user;
-    $_SESSION['position'] = $position;
-    MakeLog("Login Allowed");
+  $result = doUserAndPasswordMatch($user, $password);
+  if ($result) {
+    if (CheckInteractiveStatus($result['accountId'])) {
+      // Log in the User - Session started in Functions
+      $_SESSION['username'] = $user;
+      $_SESSION['FullName'] = $result['name'];
+      MakeLog("Login Allowed");
 
-    if (getCurrentUser($user, $position)) {
-      // Logged in successfully, Grab user's Real Name
-      if($user == $GLOBALS['ADMINUSER']) {
-        $_SESSION['FullName'] = "Admin User";
-      } else {
-        $query="SELECT ATTENDEE_ID FROM " . $AUTHENTICATIONTABLE . " WHERE LoginName='" . $user . "'";
-        $result=run_query($query);
-        $rows=mysql_fetch_assoc($result);
-
-        $query="SELECT FIRST_NAME,LAST_NAME FROM " . $ATTENDEETABLE . " WHERE ATTENDEE_ID='" . $rows['ATTENDEE_ID'] . "'";
-        $result=run_query($query);
-        $rows=mysql_fetch_assoc($result);
-
-        $_SESSION['FullName'] = $rows['FIRST_NAME'] . " " . $rows['LAST_NAME'];
-      }
-      $_SESSION['administrator'] = CheckAdminStatus($user);
       goSite('/index.php?Function=main'); // goto default
+      // ##==## On "main", do check for cron process running based on "LastCronRun" from SystemLogTable
     } else {
+      MakeLog($user . " not authorized for interaction session on this system, login denied");
+      $RETRY=2; // Not Authorized
       LogoutUser();
-      MakeLog("User not authorized for " . $position . ", logged out");
-      $RETRY=2; // Not Authorized for requested position
     }
   } else {
-    MakeLog("Failed Login Attempt");
+    MakeLog("Failed Login Attempt by " . $user);
     $RETRY=1; // Bad password
   }
 }
